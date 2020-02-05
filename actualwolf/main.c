11d81837f21109e592e3bbd25f0552dbdfd9a2ab
Timothee GONDOLO <tigondol@e2r2p4.42.fr>
2020-02-05 12:38:36 +0100

    sdl grpahic

--- actualwolf/main.c
+++ actualwolf/main.c
@@ -19,9 +19,9 @@ void init_sdl_ressources(t_enval *env)
     if(0 != SDL_Init(SDL_INIT_VIDEO))
         ft_error("Failed to Init SDL", env, 0);
     if (!(env->win = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              WIN_W, WIN_H, SDL_WINDOW_SHOWN)))
+                              WIN_W, WIN_H, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP)))
         ft_error("Failed to create Window", env, 0);
-    if (!(env->rend = SDL_CreateRenderer(env->win, -1, SDL_RENDERER_ACCELERATED)))
+    if (!(env->rend = SDL_CreateRenderer(env->win, -1, SDL_RENDERER_TARGETTEXTURE)))
         ft_error("Failed to create Renderer", env, 0);
     env->screen = SDL_CreateTexture(env->rend, SDL_PIXELFORMAT_BGRA32, SDL_TEXTUREACCESS_STREAMING, WIN_W, WIN_H);
     // env->editor_grid = SDL_CreateTexture(env->rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, GRID_SIZE_X, GRID_SIZE_Y);
