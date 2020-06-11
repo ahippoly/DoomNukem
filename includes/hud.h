e8467fa8067b78ea96dfbe6afa59e672e1e551c5
Alex Bui <alxbui14@gmail.com>
2020-06-11 15:15:55 +0200

    update hud.h

--- includes/hud.h
+++ includes/hud.h
@@ -7,48 +7,80 @@
 # include "editor.h"
 # include "SDL_ttf.h"
 
+/* Colors */
+
 # define HEALTH_BG 0x1DB100
 # define HEALTH 0x76f013
 
-// typedef struct	s_ammo
-// {
-// 	int			left;
-// 	int			capacity;
-// 	int			damage;
-// }				t_ammo;
+/* PATH */
+
+# define WEAPON_NB		6
+# define WEAPON_PATH_0	"/img/hud/handgun.bmp"
+# define WEAPON_PATH_1	"/img/hud/goldengun.bmp"
+# define WEAPON_PATH_2	"/img/hud/handgun.bmp"
+# define WEAPON_PATH_3	"/img/hud/handgun.bmp"
+# define WEAPON_PATH_4	"/img/hud/handgun.bmp"
+
+# define WEAPON_NAME_0	"LMG"
+# define WEAPON_NAME_1	"AR"
+# define WEAPON_NAME_2	"DD"
+# define WEAPON_NAME_3	"REVOLVER"
+# define WEAPON_NAME_4	"SSG"
 
 typedef struct	s_weapon
 {
-	char		*path;
+	int			id;
 	char		*name;
-	int			ammo_left;
-	int			capacity;
-	int			dammage;
-
+	int			ammo_left; // munitions restantes
+	int			capacity; // capacité max de chargement
+	int			dammage; // pts de dégat
+	int			range; // portée d'une arme
+	SDL_Surface *surface;
+	SDL_Texture *texture;
 }				t_weapon;
 
+/*
 typedef struct	s_perso
 {
 	char		*path;
 	char		*name;
 }				t_perso;
+*/
 
 typedef struct	s_hud
 {
+	int			current_weap_id;
+	t_weapon	**perso_weapon;
 	SDL_Surface *s_ammo;
 	SDL_Surface *s_perso_w;
-	SDL_Surface *ammo_text;
-	SDL_Texture *t_ammo;
 	SDL_Texture *t_perso_w;
 	TTF_Font	*font;
 	SDL_Color	color;
 }				t_hud;
 
 
-/* hud_text */
+/* hud_text.c */
+
 int				put_text(t_data *d, t_hud *hud);
 int				init_ttf(t_hud *hud);
 int				quit_ttf(t_hud *hud);
 void			display_text(t_data *d, t_hud *hud, char *message, SDL_Rect message_pos);
+
+/* hud_clean.c */
+
+void			free_hud(t_hud *hud);
+void			free_hud_weapon(t_hud *hud);
+
+/* hud_obj.c */
+
+void			init_weapons(t_hud *hud, t_data *d);
+
+/* hud_weapons_init.c */
+
+void			init_weapon_0(t_hud *hud, t_data *d);
+void			init_weapon_1(t_hud *hud, t_data *d);
+void			init_weapon_2(t_hud *hud, t_data *d);
+void			init_weapon_3(t_hud *hud, t_data *d);
+void			init_weapon_4(t_hud *hud, t_data *d);
 
 #endif
