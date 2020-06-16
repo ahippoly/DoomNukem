2662bd3d367c68eb0109314b9ac090b3181d5cc4
Alex Bui <alxbui14@gmail.com>
2020-06-16 21:57:35 +0200

    add files and proto

--- includes/hud.h
+++ includes/hud.h
@@ -11,6 +11,23 @@
 
 # define HEALTH_BG 0x1DB100
 # define HEALTH 0x76f013
+# define HUD_TEXT 0xF1D130
+
+# define BLACK 0
+# define WHITE 0xFFFFFF
+# define DAYSKY 0x2FC4EE
+# define NIGHTSKY 0x084B83
+# define DAYFLOOR 0xC5B087
+# define NIGHTFLOOR 0x081c15
+# define DAY_HUD 0x253237
+# define NIGHT_HUD 0x5500a8
+# define TITLE_HUD 0x89fc00
+# define TEXT_HUD 0xFFE529
+# define BEYOND_MAP 0x42321b
+# define FLOOR 0xC5B087
+# define RAY 0x89fc00
+# define BLUE 0xd00000
+# define BLOC_COLOR 0x1B9E77
 
 /* weapons */
 
@@ -32,18 +49,30 @@
 # define PERSO_NAME_0 "Loni"
 # define PERSO_NAME_1 "Ray"
 
+# define PERSO_F 0
+# define PERSO_M 1
+
 # define PERSO_PATH_0 "/img/hud/perso_w.bmp"
 # define PERSO_PATH_1 "/img/hud/perso_m.bmp"
 
+/* other hud icons */
+
+# define HEALTH_ICON_PATH "/img/hud/health.bmp"
+# define AMMO_ICON_PATH "/img/hud/bullet.bmp"
+
+/* inventory */
+
+# define HEALTHPACK_PATH "/img/hud/doctor.bmp"
+# define KEY_PATH "/img/hud/key.bmp"
 
 typedef struct		s_weapon
 {
 	int				id;
-	char			*name;
-	unsigned int	ammo_left; // munitions restantes
-	unsigned int	capacity; // capacité max de chargement
 	int				dammage; // pts de dégat
 	int				range; // portée d'une arme
+	int				ammo_left; // munitions restantes
+	char			*name;
+	int				capacity; // capacité max de chargement
 	SDL_Surface 	*surface;
 	SDL_Texture 	*texture;
 }					t_weapon;
@@ -56,24 +85,38 @@ typedef struct		s_perso
 	SDL_Texture		*texture;
 }					t_perso;
 
-typedef struct		s_hud_text
+typedef struct		s_inv
 {
-	SDL_Texture		*message_texture;
-}					t_hud_text;
+	SDL_Texture		*healthpack_icon_t;
+	SDL_Texture		*healthpack_info_t;
+	SDL_Texture		*key_icon_t;
+	SDL_Texture		*key_info_t;
+	SDL_Surface		*healthpack_icon_s;
+	SDL_Surface		*healthpack_info_s;
+	SDL_Surface		*key_info_s;
+	int				health_pack;
+	int				key;
+}					t_inv;
 
 typedef struct		s_hud
 {
 	int				current_weap_id;
 	int				current_perso_id;
-	t_weapon		**perso_weapon;
-	t_perso			*perso;
+	int				hp;
+	TTF_Font		*font_nb;
+	TTF_Font		*font_text;
 	SDL_Surface 	*s_perso_w;
+	SDL_Surface		*message_ammo_s;
+	SDL_Surface		*message_health_s;
+	SDL_Texture		*message_ammo_t;
+	SDL_Texture		*health_texture; //icon health
+	SDL_Texture		*ammo_texture; //icon ammo
+	SDL_Texture		*message_health_t;
 	SDL_Texture 	*t_perso_w;
-	TTF_Font		*font;
+	t_weapon		**perso_weapon;
+	t_perso			perso;
 	SDL_Color		color;
-	// t_text		*hud_text;
-	SDL_Surface		*message_surface;
-	SDL_Texture		*message_texture;
+	t_inv			inv;
 }					t_hud;
 
 
@@ -81,35 +124,63 @@ typedef struct		s_hud
 
 int					init_ttf(t_hud *hud);
 int					quit_ttf(t_hud *hud);
-void				init_ammo_text(t_data *d, t_hud *hud);
-void				set_text(t_data *d, t_hud *hud, unsigned int nb);
-void				render_text(t_data *d, t_hud *hud, SDL_Rect pos);
 
 /* hud_clean.c */
 
 void				free_hud(t_hud *hud);
 void				free_hud_weapon(t_hud *hud);
+int					error(t_hud *hud);
 
 /* hud_weapons.c */
 
-void				init_weapons(t_data *d, t_hud *hud);
-int					put_weapon(t_data *d, t_hud *hud);
+int					init_weapons(t_data *d, t_hud *hud);
+int					put_weapon_icon(t_data *d, t_hud *hud, SDL_Rect pos);
+void				set_weapon_name(t_data *d, t_hud *hud, SDL_Rect pos);
+void				render_weapon_name(t_data *d, t_hud *hud, SDL_Rect pos);
 
 /* hud_weapons_init.c */
 
-void				init_weapon_0(t_data *d, t_hud *hud);
-void				init_weapon_1(t_data *d, t_hud *hud);
-void				init_weapon_2(t_data *d, t_hud *hud);
-void				init_weapon_3(t_data *d, t_hud *hud);
-void				init_weapon_4(t_data *d, t_hud *hud);
+int					init_weapon_0(t_data *d, t_hud *hud);
+int					init_weapon_1(t_data *d, t_hud *hud);
+int					init_weapon_2(t_data *d, t_hud *hud);
+int					init_weapon_3(t_data *d, t_hud *hud);
+int					init_weapon_4(t_data *d, t_hud *hud);
 
 /* hud_perso.c */
 
-void				init_perso(t_data *d, t_hud *hud);
-int					put_perso(t_data *d, t_hud *hud);
+int					init_perso(t_data *d, t_hud *hud);
+int					put_perso_icon(t_data *d, t_hud *hud, SDL_Rect pos);
 
 /* hud_health.c */
 
-int					put_lifebar(t_data *d);
+int					init_health_icon(t_data *d, t_hud *hud);
+int					put_health_icon(t_data *d, t_hud *hud, SDL_Rect pos);
+int					set_health_info(t_data *d, t_hud *hud, int nb);
+int					render_health_info(t_data *d, t_hud *hud, SDL_Rect pos);
+
+/* hud_ammo.c */
+
+int					init_ammo_icon(t_data *d, t_hud *hud);
+int					put_ammo_icon(t_data *d, t_hud *hud, SDL_Rect pos);
+int					set_ammo_info(t_data *d, t_hud *hud, int nb);
+int					render_ammo_info(t_data *d, t_hud *hud, SDL_Rect pos);
+
+/*  hud utils */
+
+SDL_Surface			*copy_surface(t_data *d, SDL_Surface *src, t_hud *hud);
+
+/* hud_inv.c */
+
+int					init_health_pack(t_data *d, t_hud *hud);
+int					put_healthpack_icon(t_data *d, t_hud *hud, SDL_Rect pos);
+int					render_healthpack_info(t_data *d, t_hud *hud, SDL_Rect pos);
+int					set_healthpack_info(t_data *d, t_hud *hud, int nb);
+
+/* hud_inv1.c */
+
+int					init_key_icon(t_data *d, t_hud *hud);
+int					put_key_icon(t_data *d, t_hud *hud, SDL_Rect pos);
+int					set_key_info(t_data *d, t_hud *hud, int nb);
+int					render_key_info(t_data *d, t_hud *hud, SDL_Rect pos);
 
 #endif
