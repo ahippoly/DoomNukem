19af477e97c7d98a67bb3b26e0fdaf087e280ede
Alex Bui <alxbui14@gmail.com>
2020-06-30 16:20:12 +0200

    remove exec ./menu from gameover

--- src/gameover_button.c
+++ src/gameover_button.c
@@ -31,14 +31,6 @@ int		catch_over_btn_event(t_data *d, char **av)
 			d->run_game = GAME;
 	}
 	else if (is_mouse_on_target(d, set_sdl_rect(WIN_SIZE_X / 2 - 100, WIN_SIZE_Y / 2 + 100, 200, 50)) == 1) //quit
-	{
-		if (access("./menu", X_OK))
-		{
-			printf("Oh dear, something went wrong with read()! %s\n", strerror(errno));
-			exit_env(d);
-		}
-		else
-			d->run_game = MENU;
-	}
+		d->run_game = MENU;
 	return (0);
 }
