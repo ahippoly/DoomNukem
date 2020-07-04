77bd59f472d02af126d7bfe93b7ce9c1f9c085cc
ahippoly <ahippoly@student.42.fr>
2020-07-04 22:02:43 +0200

    Removed Mouse lock

--- src/game_render.c
+++ src/game_render.c
@@ -2,7 +2,7 @@
 
 int	render_game(t_data *d, int ac, char **av)
 {
-	SDL_SetRelativeMouseMode(SDL_TRUE);
+	//SDL_SetRelativeMouseMode(SDL_TRUE);
 	while (d->run_game == GAME)
 	{
 		d->time_last_frame = d->time;
