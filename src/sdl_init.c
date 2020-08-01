50660db2dad67bf86a4e267dbe56a333a7e2e088
ahippoly <ahippoly@student.42.fr>
2020-08-01 07:08:20 +0200

    Renommed Editor window

--- src/sdl_init.c
+++ src/sdl_init.c
@@ -6,7 +6,7 @@
 /*   By: ahippoly <ahippoly@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:41:23 by alebui            #+#    #+#             */
-/*   Updated: 2020/08/01 06:39:56 by ahippoly         ###   ########.fr       */
+/*   Updated: 2020/08/01 07:07:55 by ahippoly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		init_sdl_ressources(t_env *env)
 	env->win = NULL;
 	if (0 != SDL_Init(SDL_INIT_VIDEO))
 		exit_editor(env, "error : failed to init SDL");
-	if (!(env->win = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED,
+	if (!(env->win = SDL_CreateWindow("Editor", SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED, WIN_SIZE_X, WIN_SIZE_Y, SDL_WINDOW_SHOWN)))
 		exit_editor(env, "error : failed to create window");
 	if (!(env->rend = SDL_CreateRenderer(env->win, -1,
